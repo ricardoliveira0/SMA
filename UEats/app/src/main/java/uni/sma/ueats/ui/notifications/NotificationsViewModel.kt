4e2ea27bfa91cad3df37612a5abfb748fafe9f75
Ricardo <l42647@alunos.uevora.pt>
2022-06-20 03:07:54 +0100

    add: University image; test: Fragment text

--- UEats/app/src/main/java/uni/sma/ueats/ui/notifications/NotificationsViewModel.kt
+++ UEats/app/src/main/java/uni/sma/ueats/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import android.arch.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is notifications Fragment!"
     }
     val text: LiveData<String> = _text
 }
